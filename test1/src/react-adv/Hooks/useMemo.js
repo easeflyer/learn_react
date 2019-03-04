c382d7be525f2b91685b74685497e22b259f6056
ease <easenet@gmail.com>
2019-03-04 17:13:38 +0800

    添加 hooks 案例

--- test1/src/react-adv/Hooks/useMemo.js
+++ test1/src/react-adv/Hooks/useMemo.js
@@ -12,20 +12,34 @@ import ReactDOM from 'react-dom'
 可以理解为：child1 保存了，回调函数返回的结果。
 除非 [a] 发生变化，才会重新保存这个结果。
 
+改进后的理解：
+注意 child1 里面保存了结果。其中包含 <Child1 的引用。
+当 [a] 不变时。child1 保存的结果不变，因此“复杂计算”没有重新执行。
+但是 <Child1 因为保存的是类引用，因此被重新实例化，也就重新渲染了。
+这里重点理解 child1 保存了结果。
+
+
 */
+
+let ch;
 
 
 // 比如是一个复杂的组件。
-const Child1 = ({ a }) => <h2>{a}</h2>;
+const Child1 = ({ a }) => {
+  console.log("Child1 重新渲染！！！");
+  return <h2>{a}</h2>
+};
 
 
 function Parent({ a, b }) {
   // Only re-rendered if `a` changes:
 
   const child1 = React.useMemo(() => <div>
-    {console.log("child1重新计算！")}
+    {console.log("这是一个复杂计算！！！")}
     <Child1 a={b} />
   </div>, [a]);
+  console.log("是否相等：",child1===ch)
+  ch = child1;
 
   const child2 = <div>
     {console.log("child2重新计算！")}
